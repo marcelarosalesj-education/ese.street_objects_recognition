49639f53fbc9470ae172d54de00aef1fcc5d9f66
Marcela Rosales <marcelarosalesj@gmail.com>
2019-03-24 19:52:06 -0600

    Add SIFT similarity calculation
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- diff_demo/main.py
+++ diff_demo/main.py
@@ -13,7 +13,7 @@ parser.add_argument('-s', '--scale', nargs='?',
                     help='Scale of original image',
                     default=0.5)
 parser.add_argument('-a', '--algorithm', required=True,
-                    choices=['abs', 'stif'],
+                    choices=['abs', 'sift'],
                     help='Algorithm for difference comparison')
 
 def get_resized_image(image, scale=0.5):
@@ -33,36 +33,74 @@ def display_image_information(image):
 
 
 def diff_abs(image1, image2):
-    """
-    About image differentiation:
-    https://stackoverflow.com/questions/51288756/how-do-i-calculate-the-percentage-of-difference-between-two-images-using-python/5128914
-    https://stackoverflow.com/questions/843972/image-comparison-fast-algorithm
+    """ Calculates how different are two images based on Absolute difference
+
+    Args:
+        param1: image
+        param2: image
+    Returns:
+        float: percentage
+
     """
     diff_image = cv2.absdiff(image1, image2)
     diff_image = diff_image.astype(np.uint8)
     result = (np.count_nonzero(diff_image) * 100)/ diff_image.size
     return result
 
-def get_stif(image):
+def get_sift(image):
     """
-    get STIF features
+    get sift features
     """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(gray, None)
-    image = cv2.drawKeypoints(gray, kp, image)
-    cv2.imwrite('sift_keypoints_image1.jpg', image)
+    key_points = sift.detectAndCompute(gray, None)
+    image = cv2.drawKeypoints(gray, key_points, image)
     return image
 
-def diff_stif(image1, image2):
+def diff_sift(image1, image2):
+    """Calculates how similar are two images based on SIFT features
+
+    https://pysource.com/2018/07/20/detect-how-similar-two-images-are-with-opencv-and-python/
+
+    Two images are similar if they have many good points in common, but as the size of the image is
+    variant we need to calculate proportionally.
+
+    Args:
+        param1: image
+        param2: image
+    Returns:
+        float: percentage
+
     """
-    shall return the difference percentage based on stif features
-    """
-    stif_img1 = get_stif(image1)
-    stif_img2 = get_stif(image2)
-    cv2.imshow("STIF Picture 1", stif_img1)
-    cv2.imshow("STIF Picture 2", stif_img2)
-    return 0.0
+    sift = cv2.xfeatures2d.SIFT_create()
+    key_points_1, desc_1 = sift.detectAndCompute(image1, None)
+    key_points_2, desc_2 = sift.detectAndCompute(image2, None)
+    print('Key Points 1st: {}'.format(len(key_points_1)))
+    print('Key Points 2nd: {}'.format(len(key_points_2)))
+
+    index_params = dict(algorithm=0, trees=5)
+    search_params = dict()
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(desc_1, desc_2, k=2)
+
+    good_points = []
+    ratio = 0.6
+    for m, n in matches:
+        # The less the distance, the better the matches
+        if m.distance < ratio*n.distance:
+            good_points.append(m)
+    print('Good Points: {}'.format(len(good_points)))
+
+    # How similar they are
+    number_keypoints = 0
+    number_keypoints = (
+        len(key_points_1) if len(key_points_1) >= len(key_points_2)
+        else len(key_points_2))
+
+    result = cv2.drawMatches(image1, key_points_1, image2, key_points_2, good_points, None)
+    cv2.imshow('result', result)
+    return len(good_points) / number_keypoints * 100
 
 def main():
     """
@@ -88,14 +126,13 @@ def main():
     # Select difference algorithm
     switcher = {
         'abs': diff_abs,
-        'stif': diff_stif,
+        'sift': diff_sift,
     }
     result = switcher[args.algorithm](resized_img1, resized_img2)
-    print("Result is {}".format(result))
+    print("Result is {0:.2f} %".format(result))
     # Destroy windows
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main()
